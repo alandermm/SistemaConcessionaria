577b8c3328814b81b64577eca893a8831bab9c4f
Alander Machado <alandermm@hotmail.com>
2017-11-06 19:25:29 -0200

    Atualização da Documentação

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,5 +1,8 @@
 using System;
 using NetOffice.ExcelApi;
+/// <summary>
+/// Classe Pessoa
+/// </summary>
 public class Pessoa{
 
     public string documento {get; set;}
@@ -7,6 +10,11 @@ public class Pessoa{
     public string email {get; set;}
     public Endereco endereco {get; set;}
     //private String[] dados;
+
+    /// <summary>
+    /// Método para iniciar os dados do objeto da classe Pessoa
+    /// </summary>
+    /// <param name="tipoDoc">Define o tipo do cliente: "CPF" ou "CNPJ"</param>
     public void iniciarDados(String tipoDoc){
         Console.Write("Nome do Cliente: ");
         this.nome = Console.ReadLine();
@@ -24,6 +32,10 @@ public class Pessoa{
         this.endereco.bairro = Console.ReadLine();
     }
 
+    /// <summary>
+    /// Método para salvar os dados da pessoa no arquivo de cadastro
+    /// </summary>
+    /// <param name="arquivo">Path completo para o arquivo de cadastro</param>
     public void salvar(String arquivo){
         Application ex = new Application();
         int ultimaLinha = new Cadastro().getUltimaLinha(arquivo);
@@ -40,6 +52,12 @@ public class Pessoa{
         ex.Dispose();
     }
 
+    /// <summary>
+    /// Método para carregar o objeto Pessoa do arquivo de cadastro
+    /// </summary>
+    /// <param name="doc">Número do documento da Pessoa</param>
+    /// <param name="arquivo">Path completo para o arquivo de cadastro</param>
+    /// <returns></returns>
     public Pessoa carregarPessoa(Int64 doc, String arquivo){
         Application ex = new Application();
         ex.Workbooks.Open(arquivo);
